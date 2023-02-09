bd92e62eb0cec82a4e729adef2c262c00b2132c8
namakete <namakete.dev@gmail.com>
2023-02-10 01:51:02 +0300

    Added `BMP views` to output a console message.

---
+++ src/BMPViews.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) Namakete (Ilya Oberemok) <namakete.dev@gmail.com>.
+// See the LICENCE file in the repository root for full licence text.
+
+#include "headers/BMPViews.h"
+
+void View::BMPViews::message() {
+    std::cout << "Done!" << std::endl;
+}
---
+++ src/headers/BMPViews.h
@@ -0,0 +1,18 @@
+// Copyright (c) Namakete (Ilya Oberemok) <namakete.dev@gmail.com>.
+// See the LICENCE file in the repository root for full licence text.
+
+#ifndef BMP_CONVERTER_BMPVIEWS_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <utility>
+
+namespace View {
+    class BMPViews {
+    public:
+        static void message();
+    };
+}
+
+#endif
